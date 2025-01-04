d199dc33524f593da13e8512bff27fbb618bf937
oksy <qn8524kingxyf@gmail.com>
2025-01-05 04:14:16 +0800

    调整DB

--- database/group_data.py
+++ database/group_data.py
@@ -23,7 +23,9 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def set_user_avoid_at(cls, msg_type: str, msg_type_id: int, user_id: int):
         """ 设置用户禁止被at """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return
         if data is None:
             data = copy.deepcopy(cls._default_value)
         cls.set_data_by_key(key, data)
@@ -32,14 +34,18 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def is_user_avoid_at(cls, msg_type: str, msg_type_id: int, user_id: int) -> bool:
         """ 是否该用户禁止被at """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return False
         return data is not None
 
     @classmethod
     def set_user_at_punishment_ban_rule(cls, msg_type: str, msg_type_id: int, user_id: int, ban_rule: str):
         """ 设置被at后惩罚规则 """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return
         if data is None:
             data = copy.deepcopy(cls._default_value)
         data["at_punishment_ban_rule"] = ban_rule
@@ -49,7 +55,9 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def get_at_punishment_ban_time(cls, msg_type: str, msg_type_id: int, user_id: int, at_count: int) -> int:
         """ 根据当前at次数 获取封禁时间  """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return 0
         if data is None:
             return 0
         else:
@@ -65,7 +73,9 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def set_user_at_punishment_ban_text(cls, msg_type: str, msg_type_id: int, user_id: int, ban_text: str):
         """ 设置被at后惩罚内容 """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return
         if data is None:
             data = copy.deepcopy(cls._default_value)
         data["at_punishment_ban_text"] = ban_text
@@ -75,7 +85,9 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def get_user_at_punishment_ban_text(cls, msg_type: str, msg_type_id: int, user_id: int) -> str:
         """ 获取被at后惩罚内容 """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return ""
         if data is None:
             return ""
         return data["at_punishment_ban_text"]
@@ -84,7 +96,9 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def is_user_in_black_list(cls, msg_type: str, msg_type_id: int, user_id: int) -> bool:
         """ 判断用户是否在群组黑名单中 """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return False
         if data is None:
             return False
         return data["is_black"]
@@ -93,7 +107,9 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def add_user_to_black_list(cls, msg_type: str, msg_type_id: int, user_id: int):
         """ 将用户新增至群组黑名单中 """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return
         if data is None:
             data = copy.deepcopy(cls._default_value)
         data["is_black"] = True
@@ -103,7 +119,9 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def remove_user_from_black_list(cls, msg_type: str, msg_type_id: int, user_id: int):
         """ 将用户从群组黑名单中移除 """
         key = cls.generate_key(msg_type, msg_type_id, user_id)
-        data = cls.get_data_by_key(key)
+        data, is_success = cls.get_data_by_key(key)
+        if not is_success:
+            return
         if data is None:
             return
         data["is_black"] = False
@@ -113,7 +131,10 @@ class DBPluginsGroupData(DBNoCacheImplInterface):
     def get_user_black_list(cls, msg_type: str, msg_type_id: int) -> List[int]:
         """ 获取当前群组黑名单列表 """
         black_list = []
-        for key, item in cls.get_data().items():
+        db_data, is_success = cls.get_data()
+        if not is_success:
+            return black_list
+        for key, item in db_data.items():
             key_info = cls.analysis_key(key)
             if msg_type == key_info["msg_type"] and msg_type_id == key_info["msg_type_id"] and item.get("is_black"):
                 black_list.append(key_info["user_id"])
